29d08a3e88c2f3c39b41a423613468c32145b295
DEEP DEY <63514087+Deep-Dey@users.noreply.github.com>
2020-11-06 06:04:52 +0530

    Content Updated

---
+++ script/navbar.js
@@ -0,0 +1,9 @@
+function toggle(){
+    if(document.querySelector('ul').style.display === "none"){
+        document.querySelector('ul').style.display = "flex";
+    }
+    else{
+        document.querySelector('ul').style.display = "none";
+    }
+    
+};
